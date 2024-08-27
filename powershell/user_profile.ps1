845d46a005226462e8634abcde218f00b0a79e28
Yaswanth Teja Yarlagadda <93423367+yaswanthteja@users.noreply.github.com>
2024-08-27 20:07:47 +0530

    Update user_profile.ps1

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -21,6 +21,7 @@ $env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
 # Alias
 Set-Alias -Name vim -Value nvim
 Set-Alias ll ls
+Set-Alias tt tree
 Set-Alias g git
 Set-Alias grep findstr
 Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
