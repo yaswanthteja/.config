27a94967a29536ec356b5bc3a4c8774a7cc9a7ae
Yaswanth Teja Yarlagadda <93423367+yaswanthteja@users.noreply.github.com>
2025-03-25 11:19:48 +0530

    Add files via upload

---
+++ Nvim/lua/config/keymaps.lua
@@ -0,0 +1,95 @@
+-- Ensure 'craftzdog.discipline' module exists
+local success, discipline = pcall(require, "craftzdog.discipline")
+if success then
+  discipline.cowboy()
+else
+  print("Error: craftzdog.discipline module not found")
+end
+
+-- Ensure vim.keymap is initialized
+local keymap = vim.keymap or {}
+local opts = { noremap = true, silent = true }
+
+-- Do things without affecting the registers
+keymap.set("n", "x", '"_x')
+keymap.set("n", "<Leader>p", '"0p')
+keymap.set("n", "<Leader>P", '"0P')
+keymap.set("v", "<Leader>p", '"0p')
+keymap.set("n", "<Leader>c", '"_c')
+keymap.set("n", "<Leader>C", '"_C')
+keymap.set("v", "<Leader>c", '"_c')
+keymap.set("v", "<Leader>C", '"_C')
+keymap.set("n", "<Leader>d", '"_d')
+keymap.set("n", "<Leader>D", '"_D')
+keymap.set("v", "<Leader>d", '"_d')
+keymap.set("v", "<Leader>D", '"_D')
+
+-- Increment/decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- Delete a word backwards
+keymap.set("n", "dw", 'vb"_d')
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Disable continuations
+keymap.set("n", "<Leader>o", "o<Esc>^Da", opts)
+keymap.set("n", "<Leader>O", "O<Esc>^Da", opts)
+
+-- Jumplist
+keymap.set("n", "<C-m>", "<C-i>", opts)
+
+-- New tab
+keymap.set("n", "te", ":tabedit")
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+
+-- Split window
+keymap.set("n", "ss", ":split<Return>", opts)
+keymap.set("n", "sv", ":vsplit<Return>", opts)
+
+-- Move window
+keymap.set("n", "sh", "<C-w>h")
+keymap.set("n", "sk", "<C-w>k")
+keymap.set("n", "sj", "<C-w>j")
+keymap.set("n", "sl", "<C-w>l")
+
+-- Resize window
+keymap.set("n", "<C-w><left>", "<C-w><")
+keymap.set("n", "<C-w><right>", "<C-w>>")
+keymap.set("n", "<C-w><up>", "<C-w>+")
+keymap.set("n", "<C-w><down>", "<C-w>-")
+
+-- Diagnostics
+keymap.set("n", "<C-j>", function()
+  vim.diagnostic.goto_next()
+end, opts)
+
+keymap.set("n", "<leader>r", function()
+  local success, hsl = pcall(require, "craftzdog.hsl")
+  if success then
+    hsl.replaceHexWithHSL()
+  else
+    print("Error: craftzdog.hsl module not found")
+  end
+end)
+
+keymap.set("n", "<leader>i", function()
+  local success, lsp = pcall(require, "craftzdog.lsp")
+  if success then
+    lsp.toggleInlayHints()
+  else
+    print("Error: craftzdog.lsp module not found")
+  end
+end)
+
+vim.api.nvim_create_user_command("ToggleAutoformat", function()
+  local success, lsp = pcall(require, "craftzdog.lsp")
+  if success then
+    lsp.toggleAutoformat()
+  else
+    print("Error: craftzdog.lsp module not found")
+  end
+end, {})
---
+++ Nvim/lua/plugins/keymaps.lua
@@ -0,0 +1,16 @@
+-- Disable continuations
+keymap.set("n", "<Leader>o", "o<Esc>^Da", opts)
+keymap.set("n", "<Leader>O", "O<Esc>^Da", opts)
+
+-- New tab
+keymap.set("n", "te", ":tabedit")
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+-- Split window
+keymap.set("n", "ss", ":split<Return>", opts)
+keymap.set("n", "sv", ":vsplit<Return>", opts)
+-- Move window
+keymap.set("n", "sh", "<C-w>h")
+keymap.set("n", "sk", "<C-w>k")
+keymap.set("n", "sj", "<C-w>j")
+keymap.set("n", "sl", "<C-w>l")
